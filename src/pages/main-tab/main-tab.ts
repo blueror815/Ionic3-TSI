047f8edfdf7150598e31fa341ce96fcb2962e89b
blueror815 <blueror815@hotmail.com>
2017-05-26 22:50:05 +0200

    changed styles

--- src/pages/main-tab/main-tab.ts
+++ src/pages/main-tab/main-tab.ts
@@ -21,6 +21,7 @@ export class MainTabPage {
   	constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
   		this.tab1 = 'KundenPage';
     	this.tab2 = 'StartPage';
+      this.tab6 = 'InfoPage';
       this.tab7 = 'InternPage';
 
     	this.presentConfigModal();
