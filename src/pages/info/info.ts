2985b13dcd836742bf8debab20f023a951956557
blueror815 <blueror815@hotmail.com>
2017-05-28 11:53:06 +0200

    added info page

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -9,11 +9,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class InfoPage {
 
+	public items = []; 
+  	public is_active = false;
+	public img_uri:string = "assets/images/not_avalible.png";
+	public artikedb: string = "03.04.2017, 02:00 Uhr";
+	public kundendb: string = "03.04.2017, 02:00 Uhr";
+	public kategorydb: string = "03.04.2017, 02:00 Uhr";
+	public orderdb: string = "03.04.2017, 02:00 Uhr";
+	public katalogdb: string = "03.04.2017, 02:00 Uhr";
+	
+
 	constructor(public navCtrl: NavController, public navParams: NavParams) {
 	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad InfoPage');
 	}
-
 }
--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -14,11 +14,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StartPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	public items = []; 
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad StartPage');
-  }
+  	public shownGroup = null;
 
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+		this.items = [
+		    { title: "Type 1 Diabetes", description: "Type 1 diabetes is an autoimmune disease in which the body’s immune system attacks and destroys the beta cells in the pancreas that make insulin." },
+		    { title: "Multiple Sclerosis", description: "Multiple sclerosis (MS) is an autoimmune disease in which the body's immune system mistakenly attacks myelin, the fatty substance that surrounds and protects the nerve fibers in the central nervous system." },
+		    { title: "Crohn's & Colitis", description: "Crohn's disease and ulcerative colitis (UC), both also known as inflammatory bowel diseases (IBD), are autoimmune diseases in which the body's immune system attacks the intestines." },
+		    { title: "Lupus", description: "Systemic lupus erythematosus (lupus) is a chronic, systemic autoimmune disease which can damage any part of the body, including the heart, joints, skin, lungs, blood vessels, liver, kidneys and nervous system." },
+		    { title: "Rheumatoid Arthritis", description: "Rheumatoid arthritis (RA) is an autoimmune disease in which the body's immune system mistakenly begins to attack its own tissues, primarily the synovium, the membrane that lines the joints." }
+		];
+	}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad StartPage');
+	}
+
+	toggleGroup(group) {
+		if (this.isGroupShown(group)) {
+			this.shownGroup = null;
+		} else {
+			this.shownGroup = group;
+		}
+	};
+
+	isGroupShown(group) {
+		return this.shownGroup === group;
+	};
 }
